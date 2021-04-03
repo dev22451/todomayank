2357a74e415d922d15de47f7b379115a2a1abb98
mayankkumawat <mayankkumawat015@gmail.com>
2021-04-03 17:24:34 +0530

    added delete function & searchbar & applied some css

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import '../App.css'
 import { FaTrashAlt } from 'react-icons/fa';
-import { FaCheck } from 'react-icons/fa';
+import { GiCheckMark } from 'react-icons/gi';
+import { FaPencilAlt } from 'react-icons/fa';
 
 const TaskList = (props) => {
     const {todoList, handleCheck, handleDelete} = props;
@@ -15,8 +16,11 @@ const TaskList = (props) => {
                         <div className='list' key={index}>
                             <input type="checkbox" className="check" checked={isChecked} onChange={()=>{handleCheck(index)}}/>
                             {taskname}
-                            {isChecked === true && <span className="complete" style={{color:'#fff'}}><FaCheck /></span>}
-                            <span style={{float:'right'}} onClick={()=>{handleDelete(index)}}><FaTrashAlt /></span>
+                            
+                            {isChecked === true && <span style={{float:'right'}} onClick={()=>{handleDelete(index)}}><FaTrashAlt /></span>}
+                            {isChecked === true && <span className="complete" style={{color:'#fff',marginRight:'15px'}}><FaPencilAlt /></span>}
+                            {isChecked === true && <span className="complete" style={{color:'#fff',marginRight:'15px'}}><GiCheckMark /></span>}
+                            
                         </div>
                     )
                 })
--- src/Container/TodoContainer.js
+++ src/Container/TodoContainer.js
@@ -8,14 +8,35 @@ class TodoContainer extends React.Component{
             // States for inputValue & list
             inputValue: '',
             todoList: [],
+            filterText:'',
+            alerttext:''
         }
         // Binding the constructor
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
         this.handleTodoInput = this.handleTodoInput.bind(this);
         this.handleInputValue = this.handleInputValue.bind(this);
         this.handleCheck = this.handleCheck.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
     }
 
+    handleFilterTextChange(e){
+        const refList = this.state.todoList;
+        const filterText = this.state.filterText;
+        const rows = [];
+
+        this.setState({
+          filterText: e.target.value
+        });
+
+        refList.forEach((refLis) => {
+            if(refLis.taskname.indexOf(filterText) === -1){
+              return;
+            }
+            rows.push(refLis.taskname)
+        });
+        console.log(refList.concat([rows]))
+    }
+    
     handleInputValue(e){
         // changing state for inputValue
         this.setState({
@@ -36,11 +57,16 @@ class TodoContainer extends React.Component{
         // writing the logics for inserting the inputValues in todoList array
         const {todoList, inputValue} = this.state;
         let refTodoList = todoList;
-
-        refTodoList.push({
-            taskname: inputValue,
-            isChecked: false
-        })
+        if(inputValue.length>4){
+            refTodoList.push({
+                taskname: inputValue,
+                isChecked: false
+            })
+        }else {
+            this.setState({
+                alerttext:'*required 5 characters minimum'
+            })
+        }
         this.setState({
             todoList:refTodoList,
             inputValue:''
@@ -62,14 +88,17 @@ class TodoContainer extends React.Component{
     }
 
     render(){
-        const {inputValue, todoList} = this.state;
+        const {inputValue, todoList, filterText, alerttext} = this.state;
         
         return(
             <div>
                 <TodoInput 
                     inputValue={inputValue}
+                    filterText={filterText}
                     handleInputValue={this.handleInputValue}
-                    handleTodoInput={this.handleTodoInput}/>
+                    handleTodoInput={this.handleTodoInput}
+                    handleFilterTextChange={this.handleFilterTextChange}
+                    alerttext={alerttext}/>
                 
                 <TaskList todoList={todoList} handleCheck={this.handleCheck} handleDelete={this.handleDelete}/>
             </div>
